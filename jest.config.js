646500b0f520342db9df803e101409bd8575d8c5
Hironori Akaishi <ah.gm3622@gmail.com>
2018-12-01 00:02:37 +0900

    add: coverage

--- jest.config.js
+++ jest.config.js
@@ -8,5 +8,17 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     '^~/(.*)$': '<rootDir>/$1'
-  }
+  },
+  collectCoverageFrom: [
+    'components/*.{js,vue}',
+    'pages/*.{js,vue}',
+    'layouts/*.{js,vue}',
+    'store/*.{js,vue}',
+    'plugins/*.{js,vue}',
+    '!./tests/**',
+    '!**/node_modules/**',
+    '!**/.nuxt/**'
+  ],
+  coverageDirectory: './coverage/',
+  collectCoverage: true
 };
